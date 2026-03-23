396a154472045f7fd826a4285ac9b1ba1109cbc1
varya-varya-vaya <you@example.com>
2026-03-23 11:06:05 +0300

    Добавила угадай число из перво ветки во вторую

--- ugadai chislo.py
+++ ugadai chislo.py
@@ -5,12 +5,12 @@ maxn = int(input("Максимальное число диапазона: "))
 ans = random.randint(minn, maxn)
 c = 0
 while True:
-    g = int(input("Как ты думаешь какое число я загадал? "))
+    v = int(input("Как ты думаешь какое число я загадал? "))
     c += 1
 
-    if g < ans:
+    if v < ans:
         print("Моё число больше")
-    elif g >ans:
+    elif v >ans:
         print("Моё число меньше")
     else:
         print(f"Угадал! Моё число: {ans}. Попыток : {c}")
